ce6842850c32bc97720d2095f36f0a963c5ad115
bluebox <18570367466@163.com>
2020-09-28 14:42:18 +0800

    worker add ap

--- cmd/lotus-seal-worker/main.go
+++ cmd/lotus-seal-worker/main.go
@@ -224,6 +224,9 @@ var runCmd = &cli.Command{
 
 		taskTypes = append(taskTypes, sealtasks.TTFetch, sealtasks.TTCommit1, sealtasks.TTFinalize)
 
+		if cctx.Bool("addpiece") {
+			taskTypes = append(taskTypes, sealtasks.TTAddPiece)
+		}
 		if cctx.Bool("precommit1") {
 			taskTypes = append(taskTypes, sealtasks.TTAddPieceHT)
 			taskTypes = append(taskTypes, sealtasks.TTPreCommit1)
